6b7615e0aac263a7b4cd3202c4c8a8408ab2c0c1
sj-on <samhitajoshi15@gmail.com>
2021-01-01 15:47:59 +0530

    add else statement

--- montys-color.rb
+++ montys-color.rb
@@ -6,6 +6,8 @@ puts "Wow! My favorite color is #{color} also!"
 
 if color == "blue"
     puts "Wow! My favorite color is #{color} also!"
+else
+    puts "I actually don't like #{color}. They are just colors, aren't they?"
 end
 
 # Lesson 4: Using an If Statement
